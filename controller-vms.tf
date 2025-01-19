84062ab749fc56e5b5ca3bf2e1ea253dc102baa0
ali abdalla <ali.dev.sd@gmail.com>
2025-01-19 07:24:49 +0100

    configure terraform to use new provider

--- controller-vms.tf
+++ controller-vms.tf
@@ -1,7 +1,7 @@
 resource "proxmox_vm_qemu" "controler_1" {
-  name                        = "rke2-controller-1"
-  target_node                 = "pve-1"
-  protection                  = true
+  name        = "rke2-controller-1"
+  target_node = "pve-1"
+  # protection                  = true
   vmid                        = 20001
   vm_state                    = "running"
   ciuser                      = "appdepl"
@@ -26,6 +26,9 @@ resource "proxmox_vm_qemu" "controler_1" {
   force_recreate_on_change_of = "cicustom"
   ipconfig0                   = "gw=10.100.70.1,ip=10.100.70.101/24,ip6=dhcp"
   onboot                      = true
+  boot                        = "order=scsi0" # has to be the same as the OS disk of the template
+
+  ciupgrade = true
   network {
     id        = 0
     model     = "virtio"
@@ -34,15 +37,41 @@ resource "proxmox_vm_qemu" "controler_1" {
     link_down = false
     tag       = 70
   }
+
+  # Most cloud-init images require a serial device for their display
+  serial {
+    id = 0
+  }
+
+  disks {
+    scsi {
+      scsi0 {
+        # We have to specify the disk from our template, else Terraform will think it's not supposed to be there
+        disk {
+          storage = "local-lvm"
+          # The size of the disk should be at least as big as the disk in the template. If it's smaller, the disk will be recreated
+          size = "33G"
+        }
+      }
+    }
+    ide {
+      # Some images require a cloud-init disk on the IDE controller, others on the SCSI or SATA controller
+      ide1 {
+        cloudinit {
+          storage = "local-lvm"
+        }
+      }
+    }
+  }
 }
 
 
 
 resource "proxmox_vm_qemu" "controler_2" {
-  name                        = "rke2-controller-2"
-  target_node                 = "pve-2"
-  vmid                        = 20002
-  protection                  = true
+  name        = "rke2-controller-2"
+  target_node = "pve-2"
+  vmid        = 20002
+  # protection                  = true
   vm_state                    = "running"
   ciuser                      = "appdepl"
   desc                        = <<-EOT
@@ -66,6 +95,9 @@ resource "proxmox_vm_qemu" "controler_2" {
   force_recreate_on_change_of = "cicustom"
   ipconfig0                   = "gw=10.100.70.1,ip=10.100.70.102/24,ip6=dhcp"
   onboot                      = true
+  boot                        = "order=scsi0" # has to be the same as the OS disk of the template
+
+  ciupgrade = true
   network {
     id        = 0
     model     = "virtio"
@@ -73,6 +105,32 @@ resource "proxmox_vm_qemu" "controler_2" {
     firewall  = true
     link_down = false
     tag       = 70
+  }
+
+  # Most cloud-init images require a serial device for their display
+  serial {
+    id = 0
+  }
+
+  disks {
+    scsi {
+      scsi0 {
+        # We have to specify the disk from our template, else Terraform will think it's not supposed to be there
+        disk {
+          storage = "local-lvm"
+          # The size of the disk should be at least as big as the disk in the template. If it's smaller, the disk will be recreated
+          size = "33G"
+        }
+      }
+    }
+    ide {
+      # Some images require a cloud-init disk on the IDE controller, others on the SCSI or SATA controller
+      ide1 {
+        cloudinit {
+          storage = "local-lvm"
+        }
+      }
+    }
   }
 }
 
@@ -106,7 +164,10 @@ resource "proxmox_vm_qemu" "controler_3" {
   force_recreate_on_change_of = "cicustom"
   ipconfig0                   = "gw=10.100.70.1,ip=10.100.70.103/24,ip6=dhcp"
   onboot                      = true
-  protection                  = true
+  # protection                  = true
+  boot = "order=scsi0" # has to be the same as the OS disk of the template
+
+  ciupgrade = true
   network {
     id        = 0
     model     = "virtio"
@@ -115,13 +176,39 @@ resource "proxmox_vm_qemu" "controler_3" {
     link_down = false
     tag       = 70
   }
+
+  # Most cloud-init images require a serial device for their display
+  serial {
+    id = 0
+  }
+
+  disks {
+    scsi {
+      scsi0 {
+        # We have to specify the disk from our template, else Terraform will think it's not supposed to be there
+        disk {
+          storage = "local-lvm"
+          # The size of the disk should be at least as big as the disk in the template. If it's smaller, the disk will be recreated
+          size = "33G"
+        }
+      }
+    }
+    ide {
+      # Some images require a cloud-init disk on the IDE controller, others on the SCSI or SATA controller
+      ide1 {
+        cloudinit {
+          storage = "local-lvm"
+        }
+      }
+    }
+  }
 }
 
 
 output "controllers_ipconfig" {
   value = {
-    "${proxmox_vm_qemu.controler_1.name}" = proxmox_vm_qemu.controler_1.ipconfig0
     "${proxmox_vm_qemu.controler_2.name}" = proxmox_vm_qemu.controler_2.ipconfig0
+    "${proxmox_vm_qemu.controler_1.name}" = proxmox_vm_qemu.controler_1.ipconfig0
     "${proxmox_vm_qemu.controler_3.name}" = proxmox_vm_qemu.controler_3.ipconfig0
   }
 }
--- workers-vms.tf
+++ workers-vms.tf
@@ -1,32 +1,29 @@
 resource "proxmox_vm_qemu" "workers_1" {
-  name                        = "rke2-worker-1${count.index + 1}"
-  count                       = 3
-  target_node                 = "pve-1"
-  protection                  = false
-  vmid                        = 20004 + count.index
-  vm_state                    = "running"
-  ciuser                      = "appdepl"
-  desc                        = <<-EOT
-            <div align='center'><a href='https://Helper-Scripts.com'><img src='https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/images/logo-81x112.png'/></a>
-              # Ubuntu 24.04 VM
-              <a href='https://ko-fi.com/D1D7EP4GF'><img src='https://img.shields.io/badge/&#x2615;-Buy me a coffee-blue' /></a>
-              </div>
-        EOT
-  qemu_os                     = "l26"
-  full_clone                  = true
-  cores                       = 4
-  vcpus                       = 4
-  sockets                     = 1
-  memory                      = 8192
-  agent                       = 1
-  sshkeys                     = var.ssh_keys
-  tags                        = "cluster;controller;pve-1"
-  clone                       = var.template_name
-  os_type                     = "cloud-init"
-  scsihw                      = "virtio-scsi-pci"
+  name        = "rke2-worker-1${count.index + 1}"
+  count       = 3
+  target_node = "pve-1"
+  vmid        = 20004 + count.index
+  vm_state    = "running"
+  ciuser      = "appdepl"
+  qemu_os     = "l26"
+  full_clone  = true
+  cores       = 4
+  vcpus       = 4
+  sockets     = 1
+  memory      = 8192
+  agent       = 1
+  sshkeys     = var.ssh_keys
+  tags        = "cluster;worker;pve-1"
+  clone       = var.template_name
+  os_type     = "cloud-init"
+  scsihw      = "virtio-scsi-pci"
+
   force_recreate_on_change_of = "cicustom"
   ipconfig0                   = "gw=10.100.70.1,ip=10.100.70.${110 + count.index}/24,ip6=dhcp"
   onboot                      = false
+  boot                        = "order=scsi0" # has to be the same as the OS disk of the template
+
+  ciupgrade = true
   network {
     id        = 0
     model     = "virtio"
@@ -35,6 +32,33 @@ resource "proxmox_vm_qemu" "workers_1" {
     link_down = false
     tag       = 70
   }
+
+  # Most cloud-init images require a serial device for their display
+  serial {
+    id = 0
+  }
+
+  disks {
+    scsi {
+      scsi0 {
+        # We have to specify the disk from our template, else Terraform will think it's not supposed to be there
+        disk {
+          storage = "local-lvm"
+          # The size of the disk should be at least as big as the disk in the template. If it's smaller, the disk will be recreated
+          size = "33G"
+        }
+      }
+    }
+    ide {
+      # Some images require a cloud-init disk on the IDE controller, others on the SCSI or SATA controller
+      ide1 {
+        cloudinit {
+          storage = "local-lvm"
+        }
+      }
+    }
+  }
+
 }
 
 resource "proxmox_vm_qemu" "workers_2" {
@@ -58,14 +82,15 @@ resource "proxmox_vm_qemu" "workers_2" {
   memory                      = 16384
   agent                       = 1
   sshkeys                     = var.ssh_keys
-  tags                        = "cluster;controller;pve-2"
+  tags                        = "cluster;worker;pve-2"
   clone                       = var.template_name
   os_type                     = "cloud-init"
   scsihw                      = "virtio-scsi-pci"
   force_recreate_on_change_of = "cicustom"
   ipconfig0                   = "gw=10.100.70.1,ip=10.100.70.${140 + count.index}/24,ip6=dhcp"
   onboot                      = true
-  protection                  = false
+  boot                        = "order=scsi0" # has to be the same as the OS disk of the template
+  ciupgrade                   = true
   network {
     id        = 0
     model     = "virtio"
@@ -73,6 +98,32 @@ resource "proxmox_vm_qemu" "workers_2" {
     firewall  = true
     link_down = false
     tag       = 70
+  }
+
+  # Most cloud-init images require a serial device for their display
+  serial {
+    id = 0
+  }
+
+  disks {
+    scsi {
+      scsi0 {
+        # We have to specify the disk from our template, else Terraform will think it's not supposed to be there
+        disk {
+          storage = "local-lvm"
+          # The size of the disk should be at least as big as the disk in the template. If it's smaller, the disk will be recreated
+          size = "34G"
+        }
+      }
+    }
+    ide {
+      # Some images require a cloud-init disk on the IDE controller, others on the SCSI or SATA controller
+      ide1 {
+        cloudinit {
+          storage = "local-lvm"
+        }
+      }
+    }
   }
 }
 
@@ -98,14 +149,16 @@ resource "proxmox_vm_qemu" "workers_3" {
   memory                      = 16384
   agent                       = 1
   sshkeys                     = var.ssh_keys
-  tags                        = "cluster;controller;pve-3"
+  tags                        = "cluster;worker;pve-3"
   clone                       = var.template_name
   os_type                     = "cloud-init"
   scsihw                      = "virtio-scsi-pci"
   force_recreate_on_change_of = "cicustom"
   ipconfig0                   = "gw=10.100.70.1,ip=10.100.70.${160 + count.index}/24,ip6=dhcp"
   onboot                      = true
-  protection                  = false
+  boot                        = "order=scsi0" # has to be the same as the OS disk of the template
+
+  ciupgrade = true
   network {
     id        = 0
     model     = "virtio"
@@ -113,6 +166,32 @@ resource "proxmox_vm_qemu" "workers_3" {
     firewall  = true
     link_down = false
     tag       = 70
+  }
+
+  # Most cloud-init images require a serial device for their display
+  serial {
+    id = 0
+  }
+
+  disks {
+    scsi {
+      scsi0 {
+        # We have to specify the disk from our template, else Terraform will think it's not supposed to be there
+        disk {
+          storage = "local-lvm"
+          # The size of the disk should be at least as big as the disk in the template. If it's smaller, the disk will be recreated
+          size = "33G"
+        }
+      }
+    }
+    ide {
+      # Some images require a cloud-init disk on the IDE controller, others on the SCSI or SATA controller
+      ide1 {
+        cloudinit {
+          storage = "local-lvm"
+        }
+      }
+    }
   }
 }
 
